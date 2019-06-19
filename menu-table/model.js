639e043aec5214f8b0eb4e8a7000aa8205424c56
Alena <elen.malets@gmail.com>
2019-06-19 14:03:53 +0200

    models for menu, dish and type

---
+++ menu-table/model.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize')
+const sequelize = require('../db')
+import Dish from '../dish-table/model'
+
+const Menu = sequelize.define('menu',
+    {
+        date: {
+            type: Sequelize.DATEONLY,
+            allowNull: false
+        }
+    },
+    {
+        timestamps: false,
+        tableName: 'menu'
+    })
+
+Menu.belongsTo(Dish)
+module.exports = Menu
